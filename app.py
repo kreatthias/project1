42f4db026c06b60d569df442de97992a5344d5ba
matja <matthias.miro@gmail.com>
2018-10-18 00:23:29 +0200

    integrated decision tree, but lacks label encoding

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from sklearn import tree
+import numpy
 
-X = [[1],[5],[15],[76],[-4],[-14],[-1],[-500],[1989],[2],[42]]
-Y = ['p','p','p','p','m','m','m','m','p','p,','p']
+X = [[1,5,15],[76,-4,-14],[-1,-500,1989]]
+Y = ['pr','pr','mr']
 
 clf_tree = tree.DecisionTreeClassifier();
 clf_tree = clf_tree.fit(X,Y);
 
-test_data = [[2],[6],[-3]];
+test_data = numpy.array([2,4,4]).reshape(1,-1)
 
 prediction_tree = clf_tree.predict(test_data);
 
--- logic.py
+++ logic.py
@@ -2,16 +2,17 @@ import os
 import time
 import random
 from sklearn import tree
+import numpy as np
 
 def wins(a, b):
     if (a == b):
         return 0.5
     if(a == 'schere'):
-        return b == 'papier'
+        return int(b == 'papier')
     if(a == 'papier'):
-        return b == 'stein'
+        return int(b == 'stein')
     if(a == 'stein'):
-        return b == 'schere'
+        return int(b == 'schere')
     print("error in algorithm")
 
 def reacted(a, b):
@@ -22,21 +23,23 @@ model = tree.DecisionTreeClassifier()
 userWon = []
 userDid = []
 pcDid = []
-trainingSource = [] 
+trainingSource = []
 trainingTarget = []
 possibleValues = ['schere', 'stein', 'papier']
 userDoes = ''
 count = 0
 while(userDoes != 'exit'):
     pcDoes = ''
-    if (count == 0):
+    if (count < 3):
         pcDoes = possibleValues[random.randint(0,2)]
     else:
-        #predict here!
-        #model.fit(trainingSource, trainingTarget)
-        
+        X = trainingSource[0:count-2]
+        Y = trainingTarget
+        print(X, Y)
+        model.fit(X, Y)
+        prediction = model.predict(np.array(trainingSource[count-1]).reshape(1,-1))
+        print('my prediction is ' + prediction)
         pcDoes = possibleValues[random.randint(0,0)]
-
 
     invalidInput = True
     while(invalidInput):
@@ -64,15 +67,14 @@ while(userDoes != 'exit'):
         print('Runde ' + str(i + 1) + ': Du ' + str(userDid[i]) + ' ich ' + str(pcDid[i]) + ', Stand ' + str(userWon[i])) 
     print('------ HISTORY END ------')
 
-    if (count != 0):
+    if (count >= 1):
         change = userDid[count] != userDid[count-1]
         react = reacted(userDid[count], pcDid[count-1])
-        winBefore = userWon[count-1]
-        didBefore = userDid[count-1]
-        didNow = userDid[count]
-        trainingSource.append([[change],[react],[winBefore],[didBefore]])
-        trainingTarget.append(didNow)
-        print('Entering prediction mode:')
-        print(trainingSource)
-        print(trainingTarget)
+        win = userWon[count]
+        does = userDid[count]
+        trainingSource.append([[change],[react],[win],[does]])
+        if (count >=2):
+            didNow = userDid[count]
+            trainingTarget.append(didNow)
+        
     count += 1
